cd3e3f3dd23a1f3be8fbacb9af1addece472f3d7
Jerry Lin <cylin520@ntu.edu.tw>
2021-11-20 16:24:19 +0800

    Initial Commit

---
+++ Homework005/Homework005App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework005App.swift
+//  Homework005
+//
+//  Created by Chun-Yi Lin on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Homework005App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

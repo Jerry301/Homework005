cb5597925b1a112a66d88faf1297038ac215b753
Jerry Lin <cylin520@ntu.edu.tw>
2021-11-23 09:44:36 +0800

    Complete

--- Homework005/ContentView.swift
+++ Homework005/ContentView.swift
@@ -14,9 +14,8 @@ struct DrawView: UIViewRepresentable {
         imageView.alpha = 0.3
         view.addSubview(imageView)
         view.layer.addSublayer(topBall())
-        view.layer.addSublayer(wtareProof())
+        view.layer.addSublayer(waterProof())
         view.layer.addSublayer(stick())
-//        view.layer.addSublayer(stickBall())
         
         return view
     }
@@ -30,27 +29,25 @@ struct DrawView: UIViewRepresentable {
         
         return layer
     }
-    func wtareProof () -> CAShapeLayer {
+    func waterProof () -> CAShapeLayer {
         let path = UIBezierPath()
         path.move(to: CGPoint(x: 126, y: 32))
 
-        path.addQuadCurve(to: CGPoint(x: 35, y: 120), controlPoint:CGPoint(x: 52, y: 40))
-        
-        path.addQuadCurve(to: CGPoint(x: 77, y: 122), controlPoint:CGPoint(x: 60, y: 110))
-        path.addQuadCurve(to: CGPoint(x: 126, y: 122), controlPoint:CGPoint(x: 100, y: 110))
-        path.addQuadCurve(to: CGPoint(x: 175, y: 122), controlPoint:CGPoint(x: 150, y: 110))
-        path.addQuadCurve(to: CGPoint(x: 212, y: 120), controlPoint:CGPoint(x: 195, y: 110))
+        path.addQuadCurve(to: CGPoint(x: 34, y: 124), controlPoint:CGPoint(x: 52, y: 40))
+        path.addQuadCurve(to: CGPoint(x: 77, y: 124), controlPoint:CGPoint(x: 60, y: 110))
+        path.addQuadCurve(to: CGPoint(x: 126, y: 124), controlPoint:CGPoint(x: 100, y: 110))
+        path.addQuadCurve(to: CGPoint(x: 175, y: 124), controlPoint:CGPoint(x: 150, y: 110))
+        path.addQuadCurve(to: CGPoint(x: 214, y: 124), controlPoint:CGPoint(x: 195, y: 110))
         
         path.addQuadCurve(to: CGPoint(x: 126, y: 32), controlPoint:CGPoint(x: 200, y: 40))
-        path.lineJoinStyle = .round //為何沒有反應？
         path.close()
         
-        path.lineJoinStyle = .round
         let layer = CAShapeLayer()
         layer.path = path.cgPath
         layer.fillColor = UIColor.clear.cgColor
         layer.strokeColor = CGColor(red: 0, green: 0, blue: 1, alpha: 1)
         layer.lineWidth = 23
+        layer.lineJoin = .round
         
         
         return layer
@@ -65,28 +62,19 @@ struct DrawView: UIViewRepresentable {
         path.addQuadCurve(to: CGPoint(x: 110, y: 227), controlPoint:CGPoint(x: 122, y: 230))
         path.addQuadCurve(to: CGPoint(x: 94, y: 210), controlPoint:CGPoint(x: 96, y: 225))
         path.addLine(to: CGPoint(x: 94, y: 200))
-        path.lineCapStyle = .round //為何沒有反應？
 
         let layer = CAShapeLayer()
         layer.path = path.cgPath
         layer.fillColor = UIColor.clear.cgColor
         layer.strokeColor = CGColor(red: 0, green: 0, blue: 1, alpha: 1)
         layer.lineWidth = 23
+        layer.lineCap = .round
+
 
         
         return layer
     }
-    
-//    func stickBall () -> CAShapeLayer {
-//        let aDegree = CGFloat.pi / 180
-//        let path = UIBezierPath(arcCenter: CGPoint(x: 94, y: 200), radius: 11.5, startAngle: aDegree * 0, endAngle: aDegree * 360, clockwise: true)
-//
-//        let layer = CAShapeLayer()
-//        layer.path = path.cgPath
-//        layer.fillColor = CGColor(red: 0, green: 0, blue: 1, alpha: 1)
-//        return layer
-//    }
-    
+
     func updateUIView(_ uiView: UIView, context: Context) {
     }
 }
